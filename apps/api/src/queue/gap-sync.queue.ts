071cf2d8d02da86635208bbc045b6c0e0ad2f9cb
rsajawandi <rsajawandi@users.noreply.github.com>
2026-06-18 10:39:45 +0700

    chore: remove orphan queue declaration files

--- apps/api/src/queue/gap-sync.queue.ts
+++
@@ -1,5 +0,0 @@
-import { Queue } from "bullmq";
-import { connection } from "./connection";
-import { defaultJobOptions } from "./job-options";
-
-export const gapSyncQueue = new Queue("gap-sync", { connection, defaultJobOptions });
--- apps/api/src/queue/onboarding.queue.ts
+++
@@ -1,5 +0,0 @@
-import { Queue } from "bullmq";
-import { connection } from "./connection";
-import { defaultJobOptions } from "./job-options";
-
-export const onboardingQueue = new Queue("onboarding", { connection, defaultJobOptions });
